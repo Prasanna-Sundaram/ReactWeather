1e52afb3d7c9d4a072d34f93ce3de08aa4d647e8
Prasanna <prasanna.sundaram@gmail.com>
2016-06-04 09:36:18 +1200

    Added About Changes

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -14,7 +14,12 @@ var React = require('react');
 var About = (props) =>
 {
     return(
-        <h3>About Component</h3>
+
+       <div>
+         <h3>About</h3>
+         <p>This is the about page</p>
+       </div>
+
       )
 };
 
